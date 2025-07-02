1594918acb5d819aa70bdfa930331999d50c0d96
not_mandatory <berrabeh.anass@ensam-casa.ma>
2025-07-02 20:39:46 +0100

    modify config to use env

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
-class Config:
+"""class Config:
     SECRET_KEY = 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = 'mysql://root:anass@localhost/evaluate'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "jwt-super-secret"
+"""
+
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
